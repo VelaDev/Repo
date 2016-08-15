e8a878d1d31ccd622274e45b459bf6fbdb30ba00
Mohapi27 <cassino.happies@gmail.com>
2016-08-15 22:08:52 +0200

    Clean up

--- src/main/java/com/demo/test/App.java
+++ src/main/java/com/demo/test/App.java
@@ -1,21 +1,5 @@
 package com.demo.test;
 
-import com.demo.dao.impl.JavaMail;
-
-
-
-
-
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 
 /**
  * Hello world!
@@ -25,8 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String []recepients = {"cassino.happies@gmail.com"};
-       //JavaMail.sendFromGMail(recepients);
+    
     	
     	
     }

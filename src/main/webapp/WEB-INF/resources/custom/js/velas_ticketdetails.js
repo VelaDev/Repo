e319a7215192df57146642190a735f965cefa93d
King Patrick Thile <Kingzp@users.noreply.github.com>
2017-05-29 02:16:53 +0200

    Make my life easy

---
+++ src/main/webapp/WEB-INF/resources/custom/js/velas_ticketdetails.js
@@ -0,0 +1,179 @@
+/**
+ * Purpose: Interact with the html pages and create communication between server side
+ * Author : Fenya Tlala
+ * Company: Velaphanda 
+ * Last Date Modified:29-05-2017
+ */
+
+// myDatatable table -->
+$(document).ready(function() {
+	$('#myDatatable').DataTable({
+		"jQueryUI" : true,
+		"pagingType" : "full_numbers",
+		"lengthMenu" : [ [ 5, 10, 50, -1 ], [ 5, 10, 50, "All" ] ]
+	/* few more options are available to use */
+	});
+});
+
+// resolved Details table -->
+$(document).ready(function() {
+	$('#resolvededDetails').DataTable({
+		"jQueryUI" : true,
+		"pagingType" : "full_numbers",
+		"lengthMenu" : [ [ 5, 10, 50, -1 ], [ 5, 10, 50, "All" ] ]
+	/* few more options are available to use */
+	});
+});
+
+// Boot Stock Table -->
+$(document).ready(function() {
+	$('#bStock').DataTable({
+		"jQueryUI" : true,
+		"pagingType" : "full_numbers",
+		"lengthMenu" : [ [ 5, 10, 50, -1 ], [ 5, 10, 50, "All" ] ]
+	/* few more options are available to use */
+	});
+});
+
+// Site Stock Table -->
+$(document).ready(function() {
+	$('#sStock').DataTable({
+		"jQueryUI" : true,
+		"pagingType" : "full_numbers",
+		"lengthMenu" : [ [ 5, 10, 50, -1 ], [ 5, 10, 50, "All" ] ]
+	/* few more options are available to use */
+	});
+});
+
+// Show solution details when status change to resolved -->
+$('#status').change(function() {
+	var opval = $(this).val();
+	if (opval == "Resolved") {
+		$('#solutionDetails').modal("show");
+	}
+});
+
+// Take selected used part numbers only -->
+function checkUsedPartNumbers() {
+
+	var checkboxes = document.getElementsByName('selectedItem');
+	var checkboxesChecked = [];
+	// loop over them all
+	for (var i = 0; i < checkboxes.length; i++) {
+		// And stick the checked ones onto an array...
+		if (checkboxes[i].checked) {
+			checkboxesChecked.push(checkboxes[i].value);
+		}
+	}
+	document.getElementById("usedPartNumbers").value = checkboxesChecked;
+
+}
+
+// Status Selection-->
+function CheckStatus(val) {
+	var element = document.getElementById('order');
+	if (val == 'pick a status' || val == 'Replaced Part')
+		element.style.display = 'block';
+	else
+		element.style.display = 'none';
+
+	var element = document.getElementById('manager');
+	if (val == 'pick a status' || val == 'Escalated')
+		element.style.display = 'block';
+	else
+		element.style.display = 'none';
+
+}
+
+// Validate action and used part numbers -->
+$(document)
+		.ready(
+				function() {
+
+					$('#updateResolved')
+							.validate(
+									{
+										errorClass : "error-class",
+										validClass : "valid-class",
+										rules : {
+
+											actionTaken : {
+												required : true
+											},
+											usedPartNumbers : {
+												required : {
+													depends : function(element) {
+														if ($("#actionTaken")
+																.val() == "Replaced Part"
+																|| $(
+																		"#actionTaken")
+																		.val() == "Replaced toner") {
+															return true;
+														} else {
+															return false;
+														}
+													}
+												}
+											}
+										},
+										messages : {
+											usedPartNumbers : 'Used part numbers is required check boot or site stock for used part numbers',
+											actionTaken : 'Action taken is required and can not be empty'
+										}
+									});
+				});
+
+// Select Action Taken -->
+$("#actionTaken").on(
+		'change',
+		function() {
+
+			if ($(this).val() == "Replaced Part"
+					|| $(this).val() == "Replaced toner") {
+				$('input[type="radio"]:enabled').attr('disabled', true);
+				$('#BootStocked, #SiteStocked').attr('disabled', false);
+				$('textarea[name="usedPartNumbers"]:enabled').attr('disabled',
+						true);
+				$('#usedPartNumbers').attr('disabled', false);
+				console.log($(this).val());
+			} else if ($(this).val() == ""
+					|| $(this).val() == "Cleared Paper Jam"
+					|| $(this).val() == "Installed Drivers"
+					|| $(this).val() == "Configured Drivers"
+					|| $(this).val() == "Configured Printer"
+					|| $(this).val() == "User Error"
+					|| $(this).val() == "No fault Found") {
+				$('input[type="radio"]:enabled').attr('disabled', true);
+				$('#BootStocked, #SiteStocked').attr('disabled', true);
+				$('textarea[name="usedPartNumbers"]:enabled').attr('disabled',
+						true);
+				$('#usedPartNumbers').attr('disabled', true);
+				console.log($(this).val());
+			}
+		});
+
+// Select customer before showing add button-->
+function ShowCustomer(val) {
+	var element = document.getElementById('siteAdd');
+	if (val == 'pick a customer' || val == 'customer')
+		element.style.display = 'block';
+	else
+		element.style.display = 'none';
+}
+
+// Accept alphanumeric characters only -->
+function isNumber(evt) {
+	evt = (evt) ? evt : window.event;
+	var charCode = (evt.which) ? evt.which : evt.keyCode;
+	if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+		return false;
+	}
+	return true;
+}
+
+// Make all Serials numbers UpperCase -->
+function upperCaseF(a) {
+	setTimeout(function() {
+		a.value = a.value.toUpperCase();
+	}, 1);
+}

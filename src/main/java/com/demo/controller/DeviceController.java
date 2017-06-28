dcf81a50d557c3ab31019f1359ad77227a71bde1
Mohapi27 <cassino.happies@gmail.com>
2017-06-28 18:38:15 +0200

    Device update

--- src/main/java/com/demo/controller/DeviceController.java
+++ src/main/java/com/demo/controller/DeviceController.java
@@ -222,7 +222,7 @@ public class DeviceController {
 				    model.addObject("productObject", device);
 				    model.addObject("AccessoryObject", deviceBean);
 				    model.addObject("accessories", accessories);
-				    model.addObject("addAccessory", accessoriesInt.getAccessoriesList(deviceBean.getSerialNumber()));
+				    model.addObject("addAccessory", a);
 				    								
 				}
 				else{
